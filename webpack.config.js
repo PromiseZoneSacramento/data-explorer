186e887564a57819d4260cab2d20796e9a64c4be
Cliff Saporta Cheng <ccfcheng@gmail.com>
2016-06-04 13:24:55 -0700

    Add babel config for es6 transpilation

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['react']
+          presets: ['es2015', 'react']
         }
       }
     ]
